ebc51a9101a78597581a7737ac2a225ae25b5fd0
Timo Ley <timo.ley@tilera.dev>
2025-11-19 10:16:49 +0100

    feat: added notify to MutableReference

--- src/api/java/dev/tilera/cwg/api/reference/IMutableReference.java
+++ src/api/java/dev/tilera/cwg/api/reference/IMutableReference.java
@@ -1,7 +1,11 @@
 package dev.tilera.cwg.api.reference;
 
+import java.util.function.Consumer;
+
 public interface IMutableReference<T> extends IReference<T> {
 
     void set(T value);
+
+    void notify(Consumer<T> hook);
 
 }
--- src/main/java/dev/tilera/cwg/options/Pointer.java
+++ src/main/java/dev/tilera/cwg/options/Pointer.java
@@ -1,10 +1,16 @@
 package dev.tilera.cwg.options;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Consumer;
+
 import dev.tilera.cwg.api.reference.IMutableReference;
 
 public class Pointer<T> implements IMutableReference<T> {
 
     private T value;
+    private Set<Consumer<T>> hooks = new HashSet<>();
+
     public Pointer() {
         value = null;
     }
@@ -21,6 +27,12 @@ public class Pointer<T> implements IMutableReference<T> {
     @Override
     public void set(T value) {
         this.value = value;
+        hooks.forEach((hook) -> hook.accept(value));
+    }
+
+    @Override
+    public void notify(Consumer<T> hook) {
+        hooks.add(hook);
     }
 
 }
